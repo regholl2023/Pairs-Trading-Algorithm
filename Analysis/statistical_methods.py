fc5f59932b57e61e540ca37daf434efe93ff9101
Aidan Alrawi <112656616+AidanAlr@users.noreply.github.com>
2023-12-29 02:33:22 +0000

    Refactor statistical_methods.py to enhance data processing and calculations. Add detailed metrics calculation for stock data. Update test_collect_metrics_for_pair.py to improve test coverage and structure.
    Signed-off-by: Aidan Alrawi <112656616+AidanAlr@users.noreply.github.com>

--- Analysis/statistical_methods.py
+++ Analysis/statistical_methods.py
@@ -25,9 +25,48 @@ def collect_metrics_for_pair(stock_1, stock_2) -> pd.DataFrame:
     Calculates returns, forward returns, hedge ratio using rolling OLS, spread, rolling correlation, and z-score.
     Classifies z-scores into trading signals.
     """
-    stock_data_df = yf.download(tickers=[stock_1, stock_2], start=Dates.START_DATE.value, end=Dates.END_DATE.value)
+
+    # Downloading the required data
+    stock_data_df = (
+        yf.download(tickers=[stock_1, stock_2], start=Dates.START_DATE.value, end=Dates.END_DATE.value))
     stock_data_df = stock_data_df.stack()
-    # Additional processing and calculations follow...
+
+    # Finding the required metrics
+    stock_data_df['return'] = (stock_data_df['Adj Close'] - stock_data_df['Open']) / stock_data_df['Open']
+    stock_data_df['forward_return'] = stock_data_df.groupby(level=1)['return'].transform(lambda x: x.shift(-1))
+    stock_data_df = stock_data_df[['Adj Close', 'forward_return']].unstack().droplevel(axis=1, level=0)
+    stock_data_df.columns = [stock_1, stock_2, f'{stock_1}_forward_return', f'{stock_2}_forward_return']
+    stock_data_df[f'{stock_1}_return'] = np.log(stock_data_df[stock_1]).diff()
+    stock_data_df[f'{stock_2}_return'] = np.log(stock_data_df[stock_2]).diff()
+
+    # Calculating the hedge ration using a rolling OLS regression
+    stock_data_df['hedge_ratio'] = RollingOLS(stock_data_df[f'{stock_2}_return'],
+                                              stock_data_df[f'{stock_1}_return'],
+                                              window=60).fit().params.values
+
+    # Calculating the spread of stock 1 and stock 2 price
+    stock_data_df['spread'] = (stock_data_df[stock_1] - stock_data_df[stock_2] * stock_data_df['hedge_ratio'])
+
+    # Rolling Correlation test
+    stock_data_df['roll_corr'] = stock_data_df[stock_1].rolling(180).corr(stock_data_df[stock_2])
+
+    def smooth_zscore(spread):
+        return (spread.rolling(1).mean() - spread.rolling(50).mean()) / spread.rolling(50).std()
+
+    stock_data_df['z_score'] = smooth_zscore(stock_data_df['spread'])
+
+    def classify_zscore(df: pd.DataFrame) -> int:
+        if df['z_score'] < -1:
+            return 1
+        elif df['z_score'] > 1:
+            return -1
+        else:
+            return 0
+
+    # Trading Signal
+    stock_data_df['signal'] = stock_data_df.apply(classify_zscore, axis=1)
+
+    return stock_data_df.dropna()
 
 
 def adf_test(stock_1, stock_2) -> bool:
--- Tests/test_collect_metrics_for_pair.py
+++ Tests/test_collect_metrics_for_pair.py
@@ -1,71 +1,49 @@
-
 import unittest
-
 import numpy as np
 import pandas as pd
-
 from Analysis.statistical_methods import collect_metrics_for_pair
 import warnings
 
 
-def check_if_result_is_dataframe(result):
-    assert isinstance(result, pd.DataFrame), "result is not a DataFrame"
-
-
 class TestCollectMetricsForPair(unittest.TestCase):
-
-    def setUp(self):
-        self.stock_1 = 'AAPL'
-        self.stock_2 = 'MSFT'
+    @classmethod
+    def setUpClass(cls):
+        cls.stock_1 = 'AAPL'
+        cls.stock_2 = 'MSFT'
+        cls.metrics = collect_metrics_for_pair(cls.stock_1, cls.stock_2)
         warnings.filterwarnings("ignore")
 
-    #  The function should download stock data for the given stock symbols.
     def test_download_stock_data(self):
-        result = collect_metrics_for_pair(self.stock_1, self.stock_2)
-        assert isinstance(result, pd.DataFrame)
-        assert not result.empty, "DataFrame download empty: Failed to download stock data"
+        assert isinstance(self.metrics, pd.DataFrame)
+        assert not self.metrics.empty, "DataFrame download empty: Failed to download stock data"
 
-    #  The function should calculate the return and forward return for each stock.
     def test_calculate_return(self):
-        result = collect_metrics_for_pair(self.stock_1, self.stock_2)
-        # Assert 'forward_return' is a substring of one of the column names
-        assert any('forward_return' in column for column in result.columns), "forward_return not found in columns"
-        # Assert 'return' is a substring of one of the column names
-        assert any('return' in column for column in result.columns), "return not found in columns"
+        assert any('forward_return' in column for column in self.metrics.columns), "forward_return not found in columns"
+        assert any('return' in column for column in self.metrics.columns), "return not found in columns"
 
-
-    #  The function should calculate the hedge ratio using a rolling OLS regression.
     def test_calculate_hedge_ratio(self):
-        result = collect_metrics_for_pair(self.stock_1, self.stock_2)
-        assert 'hedge_ratio' in result.columns, "hedge_ratio not found in columns"
+        assert 'hedge_ratio' in self.metrics.columns, "hedge_ratio not found in columns"
 
-
-    #  The function should handle cases where the downloaded stock data has missing values.
     def test_missing_stock_data(self):
-        result = collect_metrics_for_pair(self.stock_1, self.stock_2)
-        check_if_result_is_dataframe(result)
-        assert not result.isnull().values.any(), "result contains NaN values"
+        assert isinstance(self.metrics, pd.DataFrame), "result is not a DataFrame"
+        assert not self.metrics.isnull().values.any(), "result contains NaN values"
 
-    #  The function should handle cases where the calculated hedge ratio is infinite or NaN.
     def test_infinite_hedge_ratio(self):
-        result = collect_metrics_for_pair(self.stock_1, self.stock_2)
-        check_if_result_is_dataframe(result)
-        assert not result.isin([np.inf, -np.inf, np.nan]).values.any(), "result contains infinite or NaN values"
+        assert isinstance(self.metrics, pd.DataFrame), "result is not a DataFrame"
+        assert not self.metrics.isin([np.inf, -np.inf, np.nan]).values.any(), "result contains infinite or NaN values"
 
-    #  The function should calculate the spread of stock 1 and stock 2 price.
     def test_calculate_spread(self):
-        result = collect_metrics_for_pair(self.stock_1, self.stock_2)
-        check_if_result_is_dataframe(result)
-        assert 'spread' in result.columns, "spread not found in columns"
+        assert isinstance(self.metrics, pd.DataFrame), "result is not a DataFrame"
+        assert 'spread' in self.metrics.columns, "spread not found in columns"
 
-    #  The function should calculate the rolling correlation test.
     def test_calculate_rolling_correlation(self):
-        result = collect_metrics_for_pair(self.stock_1, self.stock_2)
-        check_if_result_is_dataframe(result)
-        assert 'roll_corr' in result.columns, "roll_corr not found in columns"
+        assert isinstance(self.metrics, pd.DataFrame), "result is not a DataFrame"
+        assert 'roll_corr' in self.metrics.columns, "roll_corr not found in columns"
 
-    #  The function should calculate the z-score.
     def test_calculate_z_score(self):
-        result = collect_metrics_for_pair(self.stock_1, self.stock_2)
-        check_if_result_is_dataframe(result)
-        assert 'z_score' in result.columns, "z_score not found in columns"
+        assert isinstance(self.metrics, pd.DataFrame), "result is not a DataFrame"
+        assert 'z_score' in self.metrics.columns, "z_score not found in columns"
+
+
+if __name__ == '__main__':
+    unittest.main()
